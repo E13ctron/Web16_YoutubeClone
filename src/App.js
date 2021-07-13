5a572d2e0f207c53e0a12f122dcb3589281b9e19
Amarnath K <amarnathk5674@gmail.com>
2021-07-13 09:45:48 +0530

    Added React Router for login, signup, forgot password

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 
 import './App.css';
 import Login from "./Components/Login"
+import Signup from "./Components/Signup"
+import ForgotPassword from "./Components/ForgotPassword"
 import {AuthProvider} from "./contexts/AuthContext"
 import { Container} from "react-bootstrap"
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 function App() {
   return (
-    <AuthProvider>
+   
       <Container className="d-flex align-items-center justify-content-center"
         style={ { minHeight: "100vh" }}
         >
         <div className="App">
-          <Login />
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route path="/signup">
+                  <Signup />
+                </Route>
+                <Route path="/forgotpassword">
+                  <ForgotPassword />
+                </Route>
+                <Route path="/">
+                  <Login />
+                </Route>
+              </Switch>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
+    
   );
 }
 
