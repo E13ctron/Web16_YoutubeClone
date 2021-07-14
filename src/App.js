b69db607dad5a9d63134db920a03ed3e5764f20f
Ayush Chaudhary <acayush2002@gmail.com>
2021-07-14 11:02:43 +0530

    added googleSignIn

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-
+import React from 'react';
 import './App.css';
 import Login from "./Components/Login"
 import Signup from "./Components/Signup"
 import ForgotPassword from "./Components/ForgotPassword"
-import {AuthProvider} from "./contexts/AuthContext"
-import { Container} from "react-bootstrap"
+import { AuthProvider } from "./contexts/AuthContext"
+import { Container } from "react-bootstrap"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 function App() {
   return (
-   
-      <Container className="d-flex align-items-center justify-content-center"
-        style={ { minHeight: "100vh" }}
-        >
-        <div>
-          <Router>
-            <AuthProvider>
-              <Switch>
-                <Route exact path="/" component={Login} />
-                <Route exact path="/signup" component={Signup} />
-                <Route exact path="/forgotpassword" component={ForgotPassword} />
-              </Switch>
-            </AuthProvider>
-          </Router>
-        </div>
-      </Container>
-    
+
+    <Container className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/forgotpassword" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+
   );
 }
 
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,12 @@
 import React from "react"
+import GoogleSignIn from "./GoogleSignIn";
 
 function Login(){
     return (
-        <h1>Login Page</h1>
+        // <h1>Login Page</h1>
+        <GoogleSignIn/>
+
+        //added google sign in. Add rest login functionality on top as per need
     );
 }
 export default Login;
