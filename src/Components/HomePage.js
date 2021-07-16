9398eabc4dcbb49134ec6f1c41e135594c498724
Amarnath K <amarnathk5674@gmail.com>
2021-07-16 17:30:34 +0530

    Default body done

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 import Header from "./HomePage/Header"
+import Sidebar from "./HomePage/Sidebar"
+import VideoSection from "./HomePage/VideoSection";
+import "./HomePage/Homepage.css";
+
 
 export default function HomePage() {
     const { currentUser, signout } = useAuth()
@@ -14,6 +18,12 @@ export default function HomePage() {
     return (
         <div>
            <Header />
+           <div className="home_page">
+               <Sidebar />
+               <div className="video-section">
+                    <VideoSection />
+               </div>
+           </div>
         </div>
     )
 }
