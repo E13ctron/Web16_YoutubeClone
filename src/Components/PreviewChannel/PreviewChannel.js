3bdc712179fc313ef2b13c98c9c8e74ea6ce30b8
Ayush Chaudhary <acayush2002@gmail.com>
2021-07-22 20:00:02 +0530

    added styles.css

--- src/Components/PreviewChannel/PreviewChannel.js
+++ src/Components/PreviewChannel/PreviewChannel.js
@@ -3,8 +3,8 @@ import Header from '../Header/Header'
 import Sidebar from '../Sidebar/Sidebar'
 import "./PreviewChannel.css"
 import channel_art_photo from "../../assets/channel_art_photo.jpg"
-import { Avatar } from '@material-ui/core'
-import { Button } from '@material-ui/core'
+import { Avatar, Button } from '@material-ui/core'
+
 
 const PreviewChannel = () => {
     return (
@@ -14,16 +14,17 @@ const PreviewChannel = () => {
             <div className="preview_channel">
                 <Sidebar />
                 <div className="channel">
-                    <img className="channel_coverImg" src={channel_art_photo} alt="Channel Cover" height="422px" width="100%"></img>
+                    <img className="channel_coverImg" src={channel_art_photo} alt="Channel Cover"></img>
 
                     <div className="channel_details">
                         <div className="channel_detailsWrap">
                             <div className="channel_avatarWrap">
                                 <Avatar className="channel_avatar" />
-                                <div className="videothumb_channel">
-                                    <h1 className="videothumb_title">Captain Tony</h1>
-                                    <p className="videothumb_text watch__subCount">2M Subscribers</p>
+                                <div className="videothumb__channel">
+                                    <h1 className="channel_title">Captain Tony</h1>
+                                    <p className="videothumb__text watch__subCount">2M Subscribers</p>
                                 </div>
+                            </div>
                                 <Button className="watch__subBtn channel_subBtn" color="primary" variant="contained">SUBSCRIBE</Button>
                             </div>
                             <div className="channel_links">
@@ -32,6 +33,7 @@ const PreviewChannel = () => {
                               </div>
                               <div className="channel_link channel_link-active">
                                   <p>VIDEOS</p>
+                                  <div className="channel_link_border"/>
                               </div>
                               <div className="channel_link">
                                   <p>PLAYLISTS</p>
@@ -45,8 +47,9 @@ const PreviewChannel = () => {
 
                             </div>
 
-                        </div>
+                        
                     </div>
+                    
                 </div>
             </div>
         </div>
