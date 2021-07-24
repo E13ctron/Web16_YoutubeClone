d9d34ec50bb4681a24b000c23ddbe9826dacd5e0
Amarnath K <amarnathk5674@gmail.com>
2021-07-24 09:47:03 +0530

    Import fix

--- src/Components/Account/UploadedVideos/UploadedVideos.js
+++ src/Components/Account/UploadedVideos/UploadedVideos.js
@@ -1,4 +1,4 @@
-import React,{ useEffect } from 'react'
+import React from 'react'
 import Header from '../../Header/Header'
 import Sidebar from '../Sidebar'
 import VideoCard from '../../HomePage/VideoCard'
