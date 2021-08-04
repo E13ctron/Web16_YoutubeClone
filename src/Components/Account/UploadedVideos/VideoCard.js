7948f09ae65a88218c914584344f9fcd92389a19
Devansh-A <devaagra2210@gmail.com>
2021-08-04 23:31:56 +0530

    fixed delete video icon

--- src/Components/Account/UploadedVideos/VideoCard.js
+++ src/Components/Account/UploadedVideos/VideoCard.js
@@ -1,48 +1,65 @@
-import React from 'react';
-import { useAuth } from '../../../contexts/AuthContext'
+import React from "react";
+import { useAuth } from "../../../contexts/AuthContext";
 import moment from "moment";
 import Avatar from "@material-ui/core/Avatar";
-import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteIcon from "@material-ui/icons/Delete";
 import "./VideoCard.css";
 import { useHistory } from "react-router-dom";
 
-const VideoCard =({ video }) => {
-    
-    const history = useHistory();
-    const { setDeleteVideoOpen, setVideoDeleted } = useAuth()
-    const handleWatchVideo = () => history.push(`/watch/${video.id}`);
-    const handlePreviewChannel = () =>
+const VideoCard = ({ video }) => {
+  const history = useHistory();
+  const { setDeleteVideoOpen, setVideoDeleted } = useAuth();
+  const handleWatchVideo = () => history.push(`/watch/${video.id}`);
+  const handlePreviewChannel = () =>
     history.push(`/PreviewChannel?name=${video.email}`);
-    // console.log(video.email);
+  // console.log(video.email);
 
-    const newDate = moment.unix(video?.timestamp?.seconds).format("YYYYMMDD, HH:mm:ss");
-    const uploadedTime = moment(newDate, "YYYYMMDD, HH:mm:ss").fromNow()
-    function deleteVideoClicked(){
-        setDeleteVideoOpen(true)
-        setVideoDeleted(video)
-    }
-    return (
-        <div className="videocard">
-            <img onClick={handleWatchVideo} className="videocard_image" src={video.thumbnailURL} alt="Thumbnail" />
-            <div className="videocard_info">
-                <Avatar onClick={handlePreviewChannel}
-                    className="videoCard_avatar"
-                    src={video.channelImage}
-                    />
+  const newDate = moment
+    .unix(video?.timestamp?.seconds)
+    .format("YYYYMMDD, HH:mm:ss");
+  const uploadedTime = moment(newDate, "YYYYMMDD, HH:mm:ss").fromNow();
+  function deleteVideoClicked() {
+    setDeleteVideoOpen(true);
+    setVideoDeleted(video);
+  }
+  return (
+    <div className="videocard">
+      <img
+        onClick={handleWatchVideo}
+        className="videocard_image"
+        src={video.thumbnailURL}
+        alt="Thumbnail"
+      />
+      <div className="videocard_info">
+        <div className="videocard_info_left">
+          <Avatar
+            onClick={handlePreviewChannel}
+            className="videoCard_avatar"
+            src={video.channelImage}
+          />
 
-                <div className="videocard_channel">
-                    <h1 className="videocard_title" onClick={handleWatchVideo}>{video.title}</h1>
+          <div className="videocard_channel">
+            <h1 className="videocard_title" onClick={handleWatchVideo}>
+              {video.title}
+            </h1>
 
-                    <div className="videocard_texts">
-                        <p className="videocard_text" onClick={handlePreviewChannel}>{video.channelName}</p>
-                        <p className="videocard_text">{video.views} views • {uploadedTime}</p>
-                    </div>
-                    
-                </div>
-                <DeleteIcon onClick={deleteVideoClicked} />
+            <div className="videocard_texts">
+              <p className="videocard_text" onClick={handlePreviewChannel}>
+                {video.channelName}
+              </p>
+              <p className="videocard_text">
+                {video.views} views • {uploadedTime}
+              </p>
             </div>
+          </div>
         </div>
-    );
-}
+
+        <div className="videocard_info_right">
+          <DeleteIcon className="delete_icon" onClick={deleteVideoClicked} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default VideoCard;
