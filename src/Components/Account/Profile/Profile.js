65421331ac133243c25487b4f9945097f1d644a5
Amarnath K <amarnathk5674@gmail.com>
2021-08-06 09:21:31 +0530

    Profile Updated

--- src/Components/Account/Profile/Profile.js
+++ src/Components/Account/Profile/Profile.js
@@ -4,19 +4,35 @@ import Header from "../../Header/Header"
 import Sidebar from "../Sidebar"
 import './profile.css'
 import { useAuth } from "../../../contexts/AuthContext"
-import { database } from '../../../firebase'
+import { database, db } from '../../../firebase'
 
 function Profile() {
-    const { currentUser, currentUserData } = useAuth()
+    const { currentUser, currentUserData, videos, likedVideos } = useAuth()
     const [loading, setLoading] = useState(false)
     const nameRef = useRef()
     const [error, setError] = useState()
     const [result, setResult] = useState()
+    const [uploadCount, setUploadCount ] = useState()
+    const [ likeCount, setLikeCount ] = useState()
+    const [ subscriptionCount, setSubscriptionCount] = useState()
     useEffect(() => {
-        if(currentUserData){
-            nameRef.current.value = currentUserData.name
+        var count = 0;
+        console.log(videos)
+        for(var i = 0;i < videos.length;i++){
+            if(videos[i].email === currentUser.email){
+                count ++;
+            }
         }
-    })
+        setUploadCount(count)
+    },[videos, setUploadCount])
+    useEffect(() => {
+        setLikeCount(likedVideos.length)
+    }, [likedVideos])
+    useEffect(() => {
+        db.collection("UserSubscriptions").doc(currentUser.uid).collection("subscribedChannels").onSnapshot((snap) => {
+            setSubscriptionCount(snap.docs.length)
+        })
+    },[setSubscriptionCount, currentUser])
     function updateProfile(){
         const Name = nameRef.current.value
         if(currentUserData){
@@ -53,24 +69,33 @@ function Profile() {
            <Header />
            <div className="body">
                <Sidebar />
-               <div className="profile">
-                    <h1>Profile</h1>
-                    <div className="profile_card">
-                <Form>
-                    <Form.Group id="name" style={{margin: "20px"}}>
-                        <h4>Name</h4>
-                        <Form.Control type="text" ref={nameRef} />
-                    </Form.Group>
-                </Form>
-                { currentUser &&  <div className="email_div">
-                    <h4>Email</h4>
-                    <p>{currentUser.email}</p>
-                    <h4>User Id</h4>
-                    <p>{currentUser.uid}</p>
-                </div>}
-                <Button disabled={loading} onClick={updateProfile}>Update Profile</Button>
-                {result &&  <Alert variant="success">{result}</Alert> }
-                {error && <Alert variant="danger">{error}</Alert>}
+               <div className="profile-body">
+                    <div className="profile">
+                        <h1>Profile</h1>
+                        <div className="profile_card">
+                        <Form>
+                            <Form.Group id="name" style={{margin: "20px"}}>
+                                <h4>Name</h4>
+                                <Form.Control type="text" ref={nameRef} />
+                            </Form.Group>
+                        </Form>
+                        { currentUser &&  <div className="email_div">
+                        <h4>Email</h4>
+                        <p>{currentUser.email}</p>
+                        <h4>User Id</h4>
+                        </div>}
+                        <Button disabled={loading} onClick={updateProfile}>Update Profile</Button>
+                        {result &&  <Alert variant="success">{result}</Alert> }
+                        {error && <Alert variant="danger">{error}</Alert>}
+                    </div>
+               </div>
+               <div className="profile-video-data">
+                        <div className="profile-display">
+                            <img src={currentUser.photoURL} />
+                            <p>Uploads: {uploadCount} </p>
+                            <p>Likes: {likeCount} </p>
+                            <p>Subscriptions: {subscriptionCount} </p>
+                        </div>
                 </div>
         </div>
            </div>
