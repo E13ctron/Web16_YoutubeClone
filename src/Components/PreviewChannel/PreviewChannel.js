3bf52bda8f5228ae04abe71c90b51d96b59661db
Ayush Chaudhary <acayush2002@gmail.com>
2021-08-07 20:20:20 +0530

    removed channelart, assets, community from prevChannel

--- src/Components/PreviewChannel/PreviewChannel.js
+++ src/Components/PreviewChannel/PreviewChannel.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Header from '../Header/Header'
 import Sidebar from '../Sidebar/Sidebar'
 import "./PreviewChannel.css"
-import channel_art_photo from "../../assets/channel_art_photo.jpg"
 import { Avatar, Button } from '@material-ui/core'
 import VideoSmall from '../WatchRight/VideoSmall'
 import { useAuth } from '../../contexts/AuthContext'
@@ -96,8 +95,6 @@ const PreviewChannel = ({video}) => {
             <div className="preview_channel">
                 <Sidebar />
                 <div className="channel">
-                    <img className="channel_coverImg" src={channel_art_photo} alt="Channel Cover"></img>
-
                     <div className="channel_details">
                         <div className="channel_detailsWrap">
                             <div className="channel_avatarWrap">
@@ -115,18 +112,12 @@ const PreviewChannel = ({video}) => {
                                </> }
                             </div>
                             <div className="channel_links">
-                              {/* <div className="channel_link">
-                                  <p>HOME</p>
-                              </div> */}
                               <div className="channel_link channel_link-active">
                                   <p>VIDEOS</p>
                                   <div className="channel_link_border"/>
                               </div>
                               <div className="channel_link">
                                   <p>PLAYLISTS</p>
-                              </div>
-                              <div className="channel_link">
-                                  <p>COMMUNITY</p>
                               </div>
                               <div className="channel_link">
                                   <p>ABOUT</p>
