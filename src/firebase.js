dae2dc42ac4a2b386bfbdf2385c34a0da68c011c
mani-sahu <manisahu0912@gmail.com>
2021-08-08 18:36:06 +0530

    firebase is changed

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore"
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyC2x4HOe0JNLk-nGL0m_rJ4kzVGNlgSLhI",
-    authDomain: "fir-55fca.firebaseapp.com",
-    projectId: "fir-55fca",
-    storageBucket: "fir-55fca.appspot.com",
-    messagingSenderId: "312764874923",
-    appId: "1:312764874923:web:4d16dd6e8d7adf35bcb7ee",
-    measurementId: "G-DBHV1DH3KT"
+    apiKey: "AIzaSyAUcNy7KFH3Ie93PyMShBfjcFaCJMRHpqI",
+  authDomain: "clone-9ca26.firebaseapp.com",
+  databaseURL: "https://clone-9ca26-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "clone-9ca26",
+  storageBucket: "clone-9ca26.appspot.com",
+  messagingSenderId: "875587869400",
+  appId: "1:875587869400:web:8823ebf424183ccf1495ad",
+  measurementId: "G-GJ8F7F6G0Z"
 })
 const firestore = app.firestore()
 export const database = {
