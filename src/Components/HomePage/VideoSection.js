7ce39008b9c7a0ade7f141d946ecd3f2e456728a
Devansh-A <devaagra2210@gmail.com>
2021-08-08 19:09:49 +0530

    some css fixes

--- src/Components/HomePage/VideoSection.js
+++ src/Components/HomePage/VideoSection.js
@@ -10,7 +10,10 @@ function VideoSection(){
     const VideoComponents = videos.map(video => <VideoCard video={video} />)
     return (
         <div>
-            <h2>Recommended</h2>
+            <div className="hp-heading">
+            <div className="recommended-heading">Recommended</div>
+            </div>
+            
 
         <div className="video-section">
            {videos.length > 0 ? VideoComponents : <h2>Loading ...</h2>}
